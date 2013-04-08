d4f12fe536bb2905565271eb31320ed5f18172f6
Zivi Weinstock <zivi.weinstock@gmail.com>
2013-04-08 11:18:34 -0700

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -7,3 +7,25 @@ def to str(n,base):
 		return convert_string[n]
 	else:
 		#call the function again, append the last digit of the number
+		return toStr(n/base,base) + convert_string[n%base]
+
+def listsum(num):
+	#intialize the adder
+	sum = 0
+
+	# Initialize numList
+	numList = str(num)
+	# #iterate through each item in the number list
+	# #the sum is equal to the previous some plus the next
+	# #item in the list
+	for charNum in numList:
+		sum += int(charNum)
+	#check if sum is only one digit, if it's still 10 or
+	#more, add the digits together
+	if sum <= 9:
+		return sum 
+	else:
+		return listsum(sum)
+
+num1 = 415894671
+print "The checksum for %d is: %d" % (415894671, listsum(415894671))
